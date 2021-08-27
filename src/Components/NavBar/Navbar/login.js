d2973534d8984aa958f94f94a13feb4235ce46c7
Glen Asare <gasare1@asu.edu>
2021-08-26 21:23:55 -0700

    pushing

--- src/Components/NavBar/Navbar/login.js
+++ src/Components/NavBar/Navbar/login.js
@@ -1,56 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import axios from 'axios';
 import { Form, Button, Modal, Row, Col } from "react-bootstrap";
-export default class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleSubmit = e => {
-        e.preventDefault();
-        const data = {
-            "username": this.username,
-            "password": this.password
-        }
-        const axios = require('axios')
-        axios.post(' http://glenasare15.pythonanywhere.com/login', data)
-            .then(res => {
-                localStorage.setItem('token',res.token)
-                
-            })
-            .catch(err => {
-                console.log(err)
-            })
-            
-            
-    }
-    
-    render() {
-        return (
-            <div>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control type="username" placeholder="Enter Username"  onChange={e=>this.username = e.target.value} />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+import { Link, Redirect,useHistory } from 'react-router-dom';
+const Login = () => {
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [redirect, setRedirect] = useState(false)
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password"  onChange={e=>this.password = e.target.value} />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" value ='Submit' onClick={this.handleSubmit} >
-                        Login
-                    </Button>
-                </Form>
-            </div>
-        )
+    const login = async (e) => {
+        e.preventDefault()
+        await fetch(' http://glenasare15.pythonanywhere.com/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+
+
+                "username": username,
+                "password": password,
+
+
+            })
+        });
+
     }
+
+    return (
+        <div>
+            <Form onSubmit={login}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="username" placeholder="Enter Username" required onChange={e => setUsername(e.target.value)} />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" required onChange={e => setPassword(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit" value='Submit' onClick={login} >
+                    Login
+                </Button>
+            </Form>
+        </div>
+    )
 }
 
+export default Login
 
