5043b6e6627db082ebba610ad1cb527f91300410
gasare1 <78706814+gasare1@users.noreply.github.com>
2021-09-22 23:42:08 -0700

    Add files via upload

---
+++ src/Components/Buyer/Buyer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Buyer() {
+  return (
+    <div id="buyer" style={{ marginTop: "100px", height: "800px" }}>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSenvCb4UPSsSvITFXab_qxxcAYwn3Zg3gzo0HwZCo4cd-kTiw/viewform?embedded=true"
+        width="900"
+        height="5000"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
+}
